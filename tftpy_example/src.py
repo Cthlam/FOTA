6254b94c69d1145a54ff884a5be0b278cdc4cfef
Cthlam <thanhlam.cao99@gmail.com>
2023-07-01 23:36:17 +0700

    add FileToSend.txt, modify src.py
    Add a file to test the upload operation, modify the source file to send
    target file to server.

--- tftpy_example/src.py
+++ tftpy_example/src.py
@@ -1,9 +1,10 @@
 from tftpy import TftpClient
 
 
-Server_ip = "192.168.0.123"
-local_file = ""                             # Put filename here
+Server_ip = "192.168.1.24"
+local_file = "FileToSend.txt"               # Put filename here
+FILE = open(".\\" + local_file)
 remote_destination = "file_received.bin"    # Only given if want to save the file in a different name
 
 client = TftpClient(host=Server_ip, port=69)
-client.upload(local_file)
+client.upload(local_file,FILE)
